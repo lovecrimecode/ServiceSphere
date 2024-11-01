dc00cba5c3a980fc15c738d4346f9c26aa5aa86c
Zelidee <zelideeguemez@gmail.com>
2024-11-01 16:00:52 -0400

    arreglando

--- ServiceSphere.Web/Program.cs
+++ ServiceSphere.Web/Program.cs
@@ -1,23 +1,20 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using ServiceSphere.Application;
-using Microsoft.OpenApi.Models;
 using ServiceSphere.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
-
-    builder.Services.AddDbContext<ServiceSphereDbContext>(options =>
-   options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
-   );
+builder.Services.AddDbContext<ServiceSphereDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 builder.Services.AddScoped<EventService>();
 builder.Services.AddControllersWithViews();
 
-    var app = builder.Build();
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
@@ -30,13 +27,12 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Welcome}/{id?}");
-});
-// Mapear Razor Pages
+// Configuración de rutas usando MapControllerRoute y MapRazorPages directamente
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Welcome}/{id?}"
+);
+
 app.MapRazorPages();
 
 app.Run();
